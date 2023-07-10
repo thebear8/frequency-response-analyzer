5226edd1cd37b2afb068932e9546cac0a486baeb
thebear8 <45793990+thebear8@users.noreply.github.com>
2023-07-10 20:18:45 +0000

    Rename webpack bundle

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    filename: "bundle.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
 };

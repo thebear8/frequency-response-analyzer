8d8c1b0904e16f3017038ba00f1812f753b390b1
thebear8 <45793990+thebear8@users.noreply.github.com>
2023-07-10 21:27:32 +0000

    Begin building UI

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,36 @@
-const h1 = document.createElement("h1");
-h1.innerText = "Hello World!";
-document.body.appendChild(h1);
+document.querySelector("html")!.style.width = "100%";
+document.querySelector("html")!.style.height = "100%";
+document.querySelector("body")!.style.width = "100%";
+document.querySelector("body")!.style.height = "100%";
+
+function h1({ innerText = "" } = {}) {
+  const h1 = document.createElement("h1");
+  Object.assign(h1, { innerText });
+  return h1;
+}
+
+function button({
+  innerText = undefined as string | undefined,
+  onclick = undefined as (() => void) | undefined,
+} = {}) {
+  const button = document.createElement("button");
+  Object.assign(button, { innerText, onclick });
+  return button;
+}
+
+function div(
+  children: HTMLElement[],
+  { display = "flex", style = "flex-direction: row" } = {}
+) {
+  const container = document.createElement("div");
+  Object.assign(container, { display, style });
+  children.forEach((c) => container.appendChild(c));
+  return container;
+}
+
+document.body.appendChild(
+  div([
+    button({ innerText: "Hello World!" }),
+    button({ innerText: "Goodbye World!" }),
+  ])
+);
